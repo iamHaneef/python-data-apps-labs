7c43c6cce49de6679ca08da5fd9084659c1b01ec
iamHaneef <muhamadhaneef.official@gmail.com>
2026-04-20 17:52:41 +0530

    snowflake demo

---
+++ snowflake_app /app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+from db import fetch_data
+
+st.set_page_config(page_title="Snowflake Data", layout="wide")
+
+st.title("Worker's Data")
+
+data = fetch_data()
+
+if data:
+
+    columns = ["ID", "Name", "Age", "Salary"]
+    df = pd.DataFrame(data, columns=columns)
+
+
+    st.dataframe(df)
+
+    st.subheader("📈 Quick Stats")
+
+    col1, col2 , col3 = st.columns(3)
+
+    col1.metric("Total Workers", len(df))
+    col2.metric("Average Age", round(df["Age"].mean(),1))
+    col3.metric("Max Salary",df["Salary"].max())
+
+else:
+    st.warning("No data found")
+
+
+
---
+++ snowflake_app /db.py
@@ -0,0 +1,34 @@
+import snowflake.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_connection():
+    return snowflake.connector.connect(
+        user = os.getenv("SNOWFLAKE_USER"),
+        password = os.getenv("SNOWFLAKE_PASSWORD"),
+        account = os.getenv("SNOWFLAKE_ACCOUNT"),
+        warehouse = os.getenv("SNOWFLAKE_WAREHOUSE"),
+        database = os.getenv("SNOWFLAKE_DATABASE"),
+        schema = os.getenv("SNOWFLAKE_SCHEMA"),
+        role = os.getenv("SNOWFLAKE_ROLE")
+    )
+
+def fetch_data():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("USE WAREHOUSE demo_wh")
+    cursor.execute("USE DATABASE db")
+    cursor.execute("USE SCHEMA my_schema")
+
+    cursor.execute("SELECT * FROM worker")
+    
+    data = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return data
+
